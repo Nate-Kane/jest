3c8f3cdfe8e1eb16705fbb7fb447f49cc8bd3a32
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-02 15:20:26 -0700

    inital push

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,10 @@
 import {
   // methods,
   // methods,
-  originalArray
-} from '../app'
+  originalArray,
+  doubleArray,
+  evensOnlyArray
+} from '../app.js'
 
 // hint of look in before_each before_all
 describe('originalArray Test', ()=> {
@@ -10,3 +12,17 @@ describe('originalArray Test', ()=> {
     expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
   })
 })
+
+describe('doubleArray Test', ()=> {
+  test('doubleArray should double values in array', ()=>{
+    expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
+  })
+})
+
+// and then after doing this, you have to go into your terminal/this directory and run Yarn test
+
+// describe('evensOnlyArray Test', ()=> {
+//   test('doubleArray should double values in array', ()=>{
+//     expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
+//   })
+// })
--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 export const originalArray = (arr) =>{
   return arr
 }
+
+//doubleArray
+
+export const doubleArray = (numberArray) => {
+  return numberArray.map((num) => {
+    return num * 2;
+  })
+};
+//OR could be on one line like this     export const doubleArray = (a) => a.map((n) => {n * 2});
+
+//Here is what James' wrote down for it.
+      // numberArray = [1,2,3,4]
+      // // return array with double values in array
+      // doubleArray(numberArray) // => [2,4,6,8];  hint use map
+      // export const doubleArray = (numberArray) => {
+      //   return numberArray.map((num) => {
+      //     return num * 2;
+      //   });
+      // };
+      // Bonus
+      // export const doubleArray = (a) => a.map((n) => n * 2);
+      // export const doubleArray = (a) =>
+      //   a.map((n) => {
+      //     return n * 2;
+      //   });
+      // export const doubleArray = (a) => {
+      //   return a.map((n) => n * 2);
+      // };
+
+  export const evensOnlyArray = (numberArray) => {
+    return numberArray.filter((num) => {
+      return num % 2
+    });
+  };  // this won't run but let's get practice testing things
