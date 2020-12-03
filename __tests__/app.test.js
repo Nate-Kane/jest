27b7f5199b0c91f16db2a7672b322ba783deaf7d
Nathan Kane <kaneitalia.rana@gmail.com>
2020-12-02 19:52:39 -0700

    finished all of it

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,13 @@ import {
   // methods,
   originalArray,
   doubleArray,
-  evensOnlyArray
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray,
+  findItem,
+  sortArray
 } from '../app.js'
 
 // hint of look in before_each before_all
@@ -21,8 +27,46 @@ describe('doubleArray Test', ()=> {
 
 // and then after doing this, you have to go into your terminal/this directory and run Yarn test
 
-// describe('evensOnlyArray Test', ()=> {
-//   test('doubleArray should double values in array', ()=>{
-//     expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
-//   })
-// })
+describe('evensOnlyArray Test', ()=> {
+  test('display only even numbers', ()=>{
+    expect(evensOnlyArray([1,2,3,4])).toEqual([2,4])
+  })
+})
+
+describe('arraySum test', ()=> {
+  test('get the sum of an array of numbers', ()=>{
+    expect(arraySum([1,2,3,4])).toEqual(10)
+  })
+})
+
+describe('allNumbersGreaterThanZero Test', ()=> {
+  test('check if every number in arr is greater than zero', ()=>{
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toEqual(true)
+  })
+})
+
+describe('someNumbersAreOdd test', () => {
+  test('check if one or more of the numbers in arr are odd', ()=>{
+    expect(someNumbersAreOdd([1,2,3,4])).toEqual(true)
+  })
+})
+
+describe('evenOnlyAndDoubleArray Test', ()=> {
+  test('filter numbers to display only evens, and then double them', ()=>{
+    expect(evensOnlyAndDoubleArray([1,2,3,4])).toEqual([4,8])
+  })
+})
+
+              // OKAY so I have this working kind of, but how can I do .toEqual(1) OR false??? for an if else statement.
+              // ALSO how would I do this if I pass my findItem function TWO params, like in the HW like this (numberArray, 1)????
+describe('findItem test', ()=> {
+  test('search for one, and if not found then display "false"', ()=>{
+    expect(findItem([1,2,3,4])).toEqual(1)
+  })
+})
+
+describe('sortArray test', ()=> {
+  test('sort my array and put it in descending order', ()=>{
+    expect(sortArray([1,2,3,4])).toEqual([4,3,2,1])
+  })
+})
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// can I import another .js page here for all of my cool functions????? like funcctions.js
+
 export const originalArray = (arr) =>{
   return arr
 }
@@ -31,7 +33,72 @@ export const doubleArray = (numberArray) => {
       // };
 
   export const evensOnlyArray = (numberArray) => {
-    return numberArray.filter((num) => {
-      return num % 2
-    });
+    return numberArray.filter(evens)
   };  // this won't run but let's get practice testing things
+
+       
+
+ export const arraySum = (numberArray) => {
+   return numberArray.reduce(function(a, b){
+    return a + b;
+}, 0);
+ };
+
+ export const allNumbersGreaterThanZero = (numberArray) => {
+   // use every
+   // numberArray.every(num) if num > zero
+  return numberArray.every(bigZero);
+ }
+       
+
+ export const someNumbersAreOdd = (numberArray) => {
+   return numberArray.some(odds)
+ }
+        
+
+// export const evensOnlyAndDoubleArray = (numberArray) => {
+//    filteredArr = numberArray.filter(evens);
+//    filteredArr.map(num) {
+//      return num * 2;
+//    };
+//   }
+
+ export const evensOnlyAndDoubleArray = (numberArray) => {
+  //  let evensReturn = evens(numberArray)
+  // what I had previously was let doubledEvens = numberArray.filter(evens), and then
+      // and then in the line below I did doubledEvens.map(function (num)... etc. 
+                              //  WHY DID THIS FAIL?? It failed because the function doubledEvens isn't completed and defined until after 
+                              //  the function I placed it in would be finished, AFTER i close the bracket and parenthesis after return num * num;
+     return numberArray.filter(evens).map(function (num) {
+     return num * 2;
+   })
+    // return doubledEvens;
+ }
+
+ export const findItem = (numberArray) => {  // okay so this kinda works. I need to figure out how to pass it the second param and then log false if not found.
+      let find = numberArray.find(one => one == 1);
+        if (find == 1) {
+          return find
+        } else {
+          return false
+        }
+      }
+
+ export const sortArray = (numberArray) => {
+   return numberArray.sort(function(a,b){return b-a});
+ }
+
+
+                 /////////////////////////////   HERE ARE MY FUNCTIONS I DEFINE TO USE IN MY METHODS        ///////////////////
+
+  function odds(num) {
+      return num % 3 == 1;
+    }
+
+  function bigZero(num) {
+      return num > 0;
+    }
+  
+  function evens(num) {
+      return num % 2 == 0;
+    }
